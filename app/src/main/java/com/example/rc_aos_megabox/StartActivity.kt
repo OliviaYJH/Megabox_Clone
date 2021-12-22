e6937f20d69133e57cecd2342ee9793101fd9f49
OliviaYJH <0703olivia@naver.com>
2021-12-23 03:43:26 +0900

    add: alert Dialog

--- app/src/main/java/com/example/rc_aos_megabox/StartActivity.kt
+++ app/src/main/java/com/example/rc_aos_megabox/StartActivity.kt
@@ -1,8 +1,13 @@
 package com.example.rc_aos_megabox
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.widget.FrameLayout
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import com.example.rc_aos_megabox.databinding.ActivityStartBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -38,9 +43,32 @@ class StartActivity : AppCompatActivity() {
             )
             true
         }
+
+        binding.btnCoupon.setOnClickListener {
+            // alert dialog
+            showLoginPop()
+        }
     }
 
-    fun replaceFragment(fragment: Fragment){
+    private fun replaceFragment(fragment: Fragment){
         supportFragmentManager.beginTransaction().replace(fl.id, fragment).commit()
+    }
+
+    private fun showLoginPop(){
+        val inflater = getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val view = inflater.inflate(R.layout.alert_pop, null)
+        val textView: TextView = view.findViewById(R.id.textView)
+        textView.text = "AlertDialog"
+
+        val alertDialog = AlertDialog.Builder(this)
+            .setTitle("Dialog test")
+            .setPositiveButton("확인"){ dialog, which ->
+                Toast.makeText(applicationContext, "ok", Toast.LENGTH_SHORT).show()
+            }
+            .setNeutralButton("취소", null)
+            .create()
+
+        alertDialog.setView(view)
+        alertDialog.show()
     }
 }
